32d5dac6e4119670cd17c4c2abd7652acfea9dfe
WIlliam Zheng <williamzheng0731@gmail.com>
2025-12-10 13:16:28 -0500

    delete unecessary files

--- textbox/Textbox.gd
+++ textbox/Textbox.gd
@@ -14,7 +14,8 @@ var index := 0
 
 func _ready() -> void:
 	if $TextboxContainer.visible:
-		$"../Player".speed = 0
+		#$"../Player".speed = 0
+		pass
 
 func _input(event):
 	if event.is_action_pressed("interact"):
@@ -26,5 +27,6 @@ func show_next_line():
 		index += 1
 	else:
 		$TextboxContainer.visible = false
-		$"../Player".speed = 100
+		#$"../Player".speed = 100
+		pass
 		
