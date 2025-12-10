1db8116ab22b0ce77d1568751093ababb2703e22
KevinZhang3082 <kevinzhang3082@gmail.com>
2025-12-10 13:57:30 -0500

    boss cuz works

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 100
+@export var bullet_node: PackedScene
 
 var is_player = true
 
@@ -11,3 +12,15 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+
+func shoot():
+	var bullet = bullet_node.instantiate()
+
+	bullet.global_position = global_position
+	bullet.direction = (get_global_mouse_position() - global_position).normalized()
+
+	get_parent().call_deferred("add_child", bullet)
+ 
+func _input(event):
+	if event.is_action("shoot"):
+		shoot()
---
+++ player/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var speed = 300
+@export var bullet_node: PackedScene
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
+
+func shoot():
+	var bullet = bullet_node.instantiate()
+
+	bullet.global_position = global_position
+	bullet.direction = (get_global_mouse_position() - global_position).normalized()
+
+	get_parent().call_deferred("add_child", bullet)
+ 
+func _input(event):
+	if event.is_action("shoot"):
+		shoot()
